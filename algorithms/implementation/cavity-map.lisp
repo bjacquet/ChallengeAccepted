66780fd7f7db4ea54550496b4fda96995c7f8323
B Jacquet <bruno.jacquet@gmail.com>
2016-11-21 00:12:21 +0100

    Not the correct answer for Cavity Map

--- algorithms/implementation/cavity-map.lisp
+++ algorithms/implementation/cavity-map.lisp
@@ -1,20 +1,29 @@
 
-(defun max.digit.in.number (number)
-  (labels ((aux (num max)
-	     (if (<= num 0)
+(defun max.digit (number)
+  (labels ((max.digit.aux (num max)
+	     (if (< num 10)
 		 max
-		 (aux (floor num 10) (max max (mod num 10))))))
-    (aux number 0)))
+		 (max.digit.aux (floor num 10) (max max (mod num 10))))))
+    (max.digit.aux (floor number 10) 0)))
+
+(defun cross.cavities (lines max.cavity nr.lines)
+  (loop for line in lines
+	for l from 1 upto nr.lines
+	if (< 1 l nr.lines)
+	  collect (substitute #\X max.cavity (write-to-string line) :start 1 :end (1- nr.lines))
+	    into crossed.lines
+	else
+	  collect (write-to-string line) into crossed.lines
+	finally (return (format nil "~{~a~%~}" crossed.lines)) ))
 
 (defun cavity.map ()
   (let ((lines (read))
-	(output))
+	output)
     (flet ((digit.char (digit) (code-char (+ 48 digit))))
       (loop for l from 1 to lines
 	    for line = (read)
-	    maximizing (max.digit.in.number line) into max.cavity
-	    do         (setf output
-			     (format nil "~@[~a~%~]~a" output line))
-	    finally    (setf output
-			     (substitute #\X (digit.char max.cavity) output))))
-    (pprint output)))
+	    when (< 1 l lines)
+	      maximize (max.digit line) into max.cavity
+	    collecting line into cavity.lines
+	    finally (setf output (cross.cavities cavity.lines (digit.char max.cavity) lines))))
+    (write output)))
