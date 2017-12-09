84e30d3bd1f3723be67bc960b0d300388cb6d3ea
Bruno Jacquet <brunjact@protonmail.com>
2017-12-09 21:22:01 -0100

    Hamming in Lisp - exercism

--- exercism/common-lisp/hamming/hamming.lisp
+++ exercism/common-lisp/hamming/hamming.lisp
@@ -6,4 +6,9 @@
 
 (defun distance (dna1 dna2)
   "Number of positional differences in two equal length dna strands."
-  )
+  (when (= (length dna1) (length dna2))
+    (loop for s1 across dna1
+          for s2 across dna2
+          when (char/= s1 s2)
+            collect 1 into diffs
+          finally (return (length diffs)))))
