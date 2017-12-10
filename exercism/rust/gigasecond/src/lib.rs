2642c81b0ad846712908feb976ec2ac0e84246ec
Bruno Jacquet <brunjact@protonmail.com>
2017-12-10 11:35:31 -0100

    Gigasecond in Rust - exercism

--- exercism/rust/gigasecond/src/lib.rs
+++ exercism/rust/gigasecond/src/lib.rs
@@ -3,5 +3,7 @@ use chrono::*;
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    unimplemented!()
+    let gigaseconds: Duration = Duration::seconds(1000000000);
+    let later: DateTime<Utc> = start+gigaseconds;
+    return later;
 }
