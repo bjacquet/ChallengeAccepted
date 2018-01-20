f6f40d7f73cce850f5d16ccaf039a9124fc8c9ca
Bruno Jacquet <brunjact@protonmail.com>
2018-01-20 15:05:52 -0100

    Space Age in Haskell - exercism
    A better way of doing it.

--- exercism/haskell/space-age/src/SpaceAge.hs
+++ exercism/haskell/space-age/src/SpaceAge.hs
@@ -9,12 +9,19 @@ data Planet = Mercury
             | Uranus
             | Neptune
 
+orbitalPeriod :: Planet -> Float
+orbitalPeriod planet = case planet of
+  Mercury -> 0.24084767
+  Venus   -> 0.61519726
+  Earth   -> 1.0
+  Mars    -> 1.8808158
+  Jupiter -> 11.862615
+  Saturn  -> 29.447498
+  Uranus  -> 84.016846
+  Neptune -> 164.79132
+
+earthYears :: Float -> Float
+earthYears seconds = seconds / 31557600
+
 ageOn :: Planet -> Float -> Float
-ageOn Earth   seconds = seconds / 31557600
-ageOn Mercury seconds = (ageOn Earth seconds) / 0.24084767
-ageOn Venus   seconds = (ageOn Earth seconds) / 0.61519726
-ageOn Mars    seconds = (ageOn Earth seconds) / 1.8808158
-ageOn Jupiter seconds = (ageOn Earth seconds) / 11.862615
-ageOn Saturn  seconds = (ageOn Earth seconds) / 29.447498
-ageOn Uranus  seconds = (ageOn Earth seconds) / 84.016846
-ageOn Neptune seconds = (ageOn Earth seconds) / 164.79132
+ageOn planet seconds = (earthYears seconds) / orbitalPeriod planet
