93cb4d41cfae950b3f46bc63c1667258bdcb2068
Bruno Jacquet <brunjact@protonmail.com>
2018-01-27 23:02:44 +0000

    10 Days of JavaScript: days 0 to 5

---
+++ HackerRank/tutorials/10-days-of-javascript/day2:-loops/day2:-loops.js
@@ -0,0 +1,48 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+    var consonants = [];
+    for (var c of s){
+        if (c.search("[aeiou]") != -1)
+            console.log(c)
+        else
+            consonants.push(c);
+    }
+    for (var c of consonants) {
+        console.log(c)
+    }
+}
+
+
+function main() {
+    const s = readLine();
+
+    vowelsAndConsonants(s);
+}
