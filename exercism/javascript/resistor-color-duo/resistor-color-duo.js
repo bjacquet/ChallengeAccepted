b838838f0d5406239701c792d0a68565d735f34e
Bruno Jacquet <b.jacquet@runtime-revolution.com>
2020-03-02 17:23:52 +0000

    Resistor Color Duo in Javascript - exercism

---
+++ exercism/javascript/resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,21 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const decodedValue = ([colorA, colorB, ...args]) => {
+  return COLORS.indexOf(colorA) * 10 + COLORS.indexOf(colorB);
+};
+
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+];
